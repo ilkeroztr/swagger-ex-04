b43d39b99574a618e699a26a00bacb36b88660b9
ilkeroztr <ahmetilkerozturk@gmail.com>
2025-07-03 13:40:56 +0300

    SwaggerEx04Application.java

---
+++ src/main/java/com/swaggerex/SwaggerEx04Application.java
@@ -0,0 +1,11 @@
+package com.swaggerex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerEx04Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerEx04Application.class, args);
+    }
+}
